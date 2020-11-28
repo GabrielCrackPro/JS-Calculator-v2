5059aed1c7971eb2ee0d01d8d7eb8e97152ca6f5
GabrielCrackPro <gabrielcrackpro2001@gmail.com>
2020-11-28 13:31:22 +0100

    Bugs fixed

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,30 @@
 //Select all items
 const container = document.querySelector('.container')
 const screen = document.querySelector('.screen')
-const numbers = document.querySelectorAll('.numbers')
-const operations = document.querySelectorAll('.operations')
+
+const numbers = document.querySelectorAll('.numbers button')
+const operations = document.querySelectorAll('.operations button')
+
+const clearButton = document.querySelector('#clear-button')
+const equalButton = document.querySelector('#equal-button')
 
 numbers.forEach(number => {
-    addEventListener('click', () => {
-        screen.textContent = number.textContent
+    number.addEventListener('click', () => {
+        screen.textContent += number.textContent //Add numbers to screen
     })
 })
+operations.forEach(operation => {
+    operation.addEventListener('click', () => {
+        screen.textContent += operation.textContent //Add operations to screen
+        clear()
+    })
+})
+clearButton.addEventListener('click', () => {
+    clear()
+})
+equalButton.addEventListener('click', () => {
+
+})
+function clear() {
+    screen.textContent = null
+}
